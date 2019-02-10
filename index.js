c6d97d3a8319708d8485e5f1e466b4daaff00746
Sam <samjwilson@gmail.com>
2019-02-09 16:39:16 -0800

    add

--- index.js
+++ index.js
@@ -43,21 +43,21 @@ app.post('/move', (req, response) => {
     move: 'right' // one of: ['up','down','left','right']
   }
 
-  // if (req.you.body[0].x === 1) {
-  //   data.move = 'down';
-  // }
+  if (req.you.body[0].x === 1) {
+    data.move = 'down';
+  }
 
-  // if (req.you.body[0].x === req.board.width) {
-  //   data.move = 'up';
-  // }
+  if (req.you.body[0].x === req.board.width) {
+    data.move = 'up';
+  }
 
-  // if (req.you.body[0].y === 1) {
-  //   data.move = 'left';
-  // }
+  if (req.you.body[0].y === 1) {
+    data.move = 'left';
+  }
 
-  // if (req.you.body[0].y === req.board.height) {
-  //   data.move = 'right';
-  // }
+  if (req.you.body[0].y === req.board.height) {
+    data.move = 'right';
+  }
 
   return response.json(data.move)
 })
