dd429f57843df6afa235851fdb2b37d74e9d6feb
Yves Freypons <yves@pixelunion.net>
2019-02-24 14:03:08 -0800

    Add winston for logging

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const logger = require('morgan');
+const bodyParser = require("body-parser");
+const express = require("express");
+const winston = require("winston");
+const morgan = require("morgan");
 const {
   createGrid,
   enemyArray,
@@ -14,67 +15,91 @@ const {
   randomMove,
   snakeArray,
   updateGrid
-} = require('./helpers.js');
+} = require("./helpers.js");
 
 const {
   eat,
   followEnemyTail,
   followOwnTail,
   kill
-} = require('./behaviours.js');
+} = require("./behaviours.js");
 
 const {
   fallbackHandler,
   notFoundHandler,
   genericErrorHandler,
-  poweredByHandler,
-} = require('./handlers.js');
+  poweredByHandler
+} = require("./handlers.js");
 
 const app = express();
 
 // For deployment to Heroku, the port needs to be set using ENV, so
 // we check for the port number in process.env
-app.set('port', (process.env.PORT || 9001));
-app.enable('verbose errors');
-app.use(logger('dev'));
+app.set("port", process.env.PORT || 9001);
+app.enable("verbose errors");
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(poweredByHandler);
+let gameId;
+let logger;
 
 // Handle POST request to '/start'
-app.post('/start', (req, res) => {
+app.post("/start", (req, res) => {
+  gameId = req.body.game.id;
 
-  console.log('Yaaaasssssssss');
+  logger = winston.createLogger({
+    level: process.env.LOG_LEVEL || "debug",
+    transports: [
+      new winston.transports.Console({
+        level: "silly",
+        format: winston.format.combine(
+          winston.format.colorize({ all: true }),
+          winston.format.prettyPrint(),
+          winston.format.printf(info => `${info.message}`)
+        )
+      }),
+      new winston.transports.File({
+        level: "silly",
+        filename: `./logs/${gameId}`,
+        handleExceptions: true,
+        maxsize: 5242880, // 5MB
+        maxFiles: 5,
+        format: winston.format.printf(info => `${info.message}`)
+      })
+    ]
+  });
+  logger.info(`===================NEW GAME==================`);
+  logger.info(`game ID: ${gameId}`);
+  logger.info(`Link: https://play.battlesnake.io/g/${gameId}`);
+  logger.info(`===================NEW GAME==================`);
 
   const data = {
-    color: '#770001'
+    color: "#770001"
   };
 
   return res.json(data);
 });
 
 // Handle POST request to '/move'
-app.post('/move', (req, res) => {
+app.post("/move", (req, res) => {
+  logger.info(
+    `TURN-------------------------------------------------:, ${req.body.turn}`
+  );
   const { board, you: ourSnake } = req.body;
-  const { 
-    height,
-    width,
-    food,
-    snakes: allSnakes,
-  } = board;
+  const { height, width, food, snakes: allSnakes } = board;
   const ourHead = ourSnake.body[0];
   const ourTail = ourSnake.body[ourSnake.body.length - 1];
   const ourLength = ourSnake.body.length;
 
-  const left = { x: ourHead.x - 1, y: ourHead.y, move: 'left' };
-  const right = { x: ourHead.x + 1, y: ourHead.y, move: 'right' };
-  const up = { x: ourHead.x, y: ourHead.y - 1, move: 'up' };
-  const down = { x: ourHead.x, y: ourHead.y + 1, move: 'down' };
+  const left = { x: ourHead.x - 1, y: ourHead.y, move: "left" };
+  const right = { x: ourHead.x + 1, y: ourHead.y, move: "right" };
+  const up = { x: ourHead.x, y: ourHead.y - 1, move: "up" };
+  const down = { x: ourHead.x, y: ourHead.y + 1, move: "down" };
   const possibleDirections = [left, right, up, down];
 
   const nearestFood = findNearestFood(ourHead, food);
   const enemies = enemyArray(allSnakes, ourSnake);
   const grid = createGrid(height, width, ourSnake, enemies);
-  console.log(enemies);
   const pathObject = {
     grid: grid,
     start: ourHead,
@@ -84,21 +109,21 @@ app.post('/move', (req, res) => {
     allSnakes: allSnakes,
     height: height,
     width: width
-  }
-  
+  };
+
   let pathToFood = eat(pathObject, nearestFood);
   let pathToOwnTail = followOwnTail(pathObject, ourTail);
   let pathToEnemyTail = followEnemyTail(pathObject, enemies);
   let nextMove = false;
 
   if (nearestFood && pathToFood) {
-    console.log('pathToFood');
+    logger.info("pathToFood");
     nextMove = pathToFood;
   } else if (pathToOwnTail) {
-    console.log('pathToOwnTail');
+    logger.info("pathToOwnTail");
     nextMove = pathToOwnTail;
   } else if (pathToEnemyTail) {
-    console.log('pathToEnemyTail');
+    logger.info("pathToEnemyTail");
     nextMove = pathToEnemyTail;
   }
 
@@ -109,13 +134,13 @@ app.post('/move', (req, res) => {
     pathToEnemyTail = followEnemyTail(pathObject, enemies);
 
     if (nearestFood && pathToFood) {
-      console.log('newgrid food');
+      logger.info("newgrid food");
       nextMove = pathToFood;
     } else if (pathToOwnTail) {
-      console.log('newgrid ownTail');
+      logger.info("newgrid ownTail");
       nextMove = pathToOwnTail;
     } else if (pathToEnemyTail) {
-      console.log('newgrid enemyTail');
+      logger.info("newgrid enemyTail");
       nextMove = pathToEnemyTail;
     }
   }
@@ -126,30 +151,30 @@ app.post('/move', (req, res) => {
 
   const data = {
     move: nextMove
-  }
+  };
 
-  console.log('Sending to server: ' + JSON.stringify(data));
+  console.log("Sending to server: " + JSON.stringify(data));
 
   return res.json(data);
 });
 
-app.post('/end', (req, res) => {
+app.post("/end", (req, res) => {
   // NOTE: Any cleanup when a game is complete.
 
-  return res.json({})
-})
+  return res.json({});
+});
 
-app.post('/ping', (req, res) => {
+app.post("/ping", (req, res) => {
   // Used for checking if this snake is still alive.
   return res.json({});
-})
+});
 
 // --- SNAKE LOGIC GOES ABOVE THIS LINE ---
 
-app.use('*', fallbackHandler)
-app.use(notFoundHandler)
-app.use(genericErrorHandler)
+app.use("*", fallbackHandler);
+app.use(notFoundHandler);
+app.use(genericErrorHandler);
 
-app.listen(app.get('port'), () => {
-  console.log('Server listening on port %s', app.get('port'))
-})
+app.listen(app.get("port"), () => {
+  console.log("Server listening on port %s", app.get("port"));
+});
